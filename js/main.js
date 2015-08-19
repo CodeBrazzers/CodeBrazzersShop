dad3a6973c2098b574ef1907099cc43de8d02580
alexZogich <alex.zogich@yandex.ru>
2015-08-19 08:46:19 +0300

    added searchField

--- js/main.js
+++ js/main.js
@@ -9,6 +9,9 @@ requirejs.config({
 require(
     ['search'],
     function(Search){
-        var searchButton =  document.getElementById("searchButton");
+        var searchButton = document.getElementById("searchButton"),
+			searchField  = document.getElementById("searchField");
+
+        Search.init(searchButton,searchField);
     }
 );
--- js/modules/search.js
+++ js/modules/search.js
@@ -1,12 +1,30 @@
 define('search', ['db'], function(db){
-    var searchInput = document.createElement("input");
+    Search = {};
 
-    function initSearchButton(element){
-    	var element = element;
-    	element.parentElement.insertBefore(searchInput, element);
+    Search.isActive = false;
+
+    Search.init = function(searchButton, searchField) {
+    	var self = this;
+
+    	self.button = searchButton;
+    	self.field  = searchField;
+    	
+    	self.button.addEventListener("click", function () {
+    		self.toggle();
+    		console.log(self.isActive);
+    	});
     }
 
-    return {
-    	initSearchButton : initSearchButton
+    Search.toggle = function () {
+
+    	if (this.isActive) {
+    		this.field.style.width = '0px';
+    		this.isActive = false; 
+    	} else {
+    		this.field.style.width = '150px';
+    		this.isActive = true;
+    	}
     }
+
+    return Search;
 });
